90a87232d0502940e2471df5a4ed9fc23e365520
harrybaird <115707362+harrybaird@users.noreply.github.com>
2022-11-30 14:21:29 +0000

    Update coursework.py
    cut after 2010

--- coursework.py
+++ coursework.py
@@ -3,13 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('https://raw.githubusercontent.com/jivizcaino/PWT_10.0/main/pwt100.csv', encoding="latin-1")
-df_subset = df[['country', 'year','pop', 'cgdpo', 'emp', 'avh', 'hc', 'labsh']]
+df_subset = df[['country', 'year', 'pop', 'cgdpo', 'emp', 'avh', 'hc', 'labsh']]
+df_subset = df_subset.query('year>=2010')
 
 #remove entries with null values
 dfc = df_subset.dropna()
 
-group=dfc.groupby(['year'])
-group=group.count()
-print(group.idxmax(), group.query('2001'))
-#the most recent year with the most observations is 2001, with 64
-dfc = dfc.query('year == 2001')
+group = dfc.groupby(['year'])
+group = group.count()
+print(group[::-1].idxmax())
+#the most recent year with the most observations is 2019, with 61
+dfc = dfc.query('year == 2019')
