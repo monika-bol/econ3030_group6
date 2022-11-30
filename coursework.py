634b1c91d80ccca91bfbc17a35610463d525896c
harrybaird <115707362+harrybaird@users.noreply.github.com>
2022-11-30 15:08:01 +0000

    Update coursework.py

--- coursework.py
+++ coursework.py
@@ -3,11 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('https://raw.githubusercontent.com/jivizcaino/PWT_10.0/main/pwt100.csv', encoding="latin-1")
-df_subset = df[['country', 'year', 'pop', 'cgdpo', 'emp', 'avh', 'hc', 'labsh']]
+df_subset = df[['country', 'countrycode', 'year', 'pop', 'cgdpo', 'emp', 'avh', 'hc', 'labsh', 'ctfp']]
 df_subset = df_subset.query('year>=2010')
 
 #remove entries with null values
 dfc = df_subset.dropna()
+print(dfc.describe())
 
 group = dfc.groupby(['year'])
 group = group.count()
@@ -23,8 +24,9 @@ calc_table['yphw'] = calc_table['ypw']/dfc['avh']
 calc_table['yphc'] = dfc['cgdpo']/dfc['hc']
 calc_table['yphhc'] = calc_table['yphw']/dfc['hc']
 calc_table = calc_table.round(decimals=2)
+print(calc_table.describe(percentiles = [0.95, 0.9, 0.1, 0.05]))
+
 table = calc_table.quantile([1, 0.95, 0.9, 0.1, 0.05, 0])
-print(table)
 ypc_ratios = [round(table.iloc[0]['ypc']/table.iloc[5]['ypc']), round(table.iloc[1]['ypc']/table.iloc[4]['ypc']), round(table.iloc[2]['ypc']/table.iloc[3]['ypc'])]
 ypw_ratios = [round(table.iloc[0]['ypw']/table.iloc[5]['ypw']), round(table.iloc[1]['ypw']/table.iloc[4]['ypw']), round(table.iloc[2]['ypw']/table.iloc[3]['ypw'])]
 yphw_ratios = [round(table.iloc[0]['yphw']/table.iloc[5]['yphw']), round(table.iloc[1]['yphw']/table.iloc[4]['yphw']), round(table.iloc[2]['yphw']/table.iloc[3]['yphw'])]
