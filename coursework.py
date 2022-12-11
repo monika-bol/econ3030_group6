a0864a4c2d6910296fd2e7f75200cb27c65cf715
monika-bol <leymb23@nottingham.ac.uk>
2022-12-11 16:44:10 +0000

    question 5

--- coursework.py
+++ coursework.py
@@ -31,28 +31,97 @@ calc_table['ypw'] = ypw
 calc_table['yphw'] = yphw
 calc_table['yphc'] = yphc
 calc_table['yphhc'] = yphhc
+
 calc_table = calc_table.round(decimals=2)
 print(calc_table.describe(percentiles = [0.95, 0.9, 0.1, 0.05]))
 
 table = calc_table.quantile([1, 0.95, 0.9, 0.1, 0.05, 0])
-def ratio(y):
-    y_ratios = [round(table.iloc[0][y]/table.iloc[5][y]), round(table.iloc[1][y]/table.iloc[4][y]), round(table.iloc[2][y]/table.iloc[3][y])]
-    return (y_ratios)
+ypc_ratios = [round(table.iloc[0]['ypc']/table.iloc[5]['ypc']), round(table.iloc[1]['ypc']/table.iloc[4]['ypc']), round(table.iloc[2]['ypc']/table.iloc[3]['ypc'])]
+ypw_ratios = [round(table.iloc[0]['ypw']/table.iloc[5]['ypw']), round(table.iloc[1]['ypw']/table.iloc[4]['ypw']), round(table.iloc[2]['ypw']/table.iloc[3]['ypw'])]
+yphw_ratios = [round(table.iloc[0]['yphw']/table.iloc[5]['yphw']), round(table.iloc[1]['yphw']/table.iloc[4]['yphw']), round(table.iloc[2]['yphw']/table.iloc[3]['yphw'])]
+yphc_ratios = [round(table.iloc[0]['yphc']/table.iloc[5]['yphc']), round(table.iloc[1]['yphc']/table.iloc[4]['yphc']), round(table.iloc[2]['yphc']/table.iloc[3]['yphc'])]
+yphhc_ratios = [round(table.iloc[0]['yphhc']/table.iloc[5]['yphhc']), round(table.iloc[1]['yphhc']/table.iloc[4]['yphhc']), round(table.iloc[2]['yphhc']/table.iloc[3]['yphhc'])]
 
-ratio_table = pd.DataFrame(list(zip(ratio('ypc'), ratio('ypw'), ratio('yphw'), ratio('yphc'), ratio('yphhc'))), columns = ['ypc', 'ypw', 'yphw', 'yphc', 'yphhc'], index = ['rich/poor', '95/5', '90/10'] )
+ratio_table = pd.DataFrame(list(zip(ypc_ratios, ypw_ratios, yphw_ratios, yphc_ratios, yphhc_ratios)), columns = ['ypc', 'ypw', 'yphw', 'yphc', 'yphhc'])
 print(ratio_table)
-
-def log_var(a):
-    return(np.log(calc_table[a].var()))
-
-log_var_table = pd.DataFrame(data = (log_var('ypc'), log_var('ypw'), log_var('yphw'), log_var('yphc'), log_var('yphhc')), columns = ['log variance'], index = ['ypc', 'ypw', 'yphw', 'yphc', 'yphhc'])
-print(log_var_table)
-
-#4
 
 #5
 
-
+plt.subplot(5,5,1)
 plt.scatter(np.log(ypw), dfc.avh)
+
+plt.subplot(5,5,2)
+plt.scatter(np.log(ypw), dfc.cn)
+
+plt.subplot(5,5,3)
+plt.scatter(np.log(ypw), dfc.hc)
+
+plt.subplot(5,5,4)
+plt.scatter(np.log(ypw), 1-dfc['labsh'])
+
+plt.subplot(5,5,5)
+plt.scatter(np.log(ypw), dfc.ctfp)
+
+plt.subplot(5,5,6)
+plt.scatter(np.log(yphw), dfc.avh)
+
+plt.subplot(5,5,7)
+plt.scatter(np.log(yphw), dfc.cn)
+
+plt.subplot(5,5,8)
+plt.scatter(np.log(yphw), dfc.hc)
+
+plt.subplot(5,5,9)
+plt.scatter(np.log(yphw), 1-dfc['labsh'])
+
+plt.subplot(5,5,10)
+plt.scatter(np.log(yphw), dfc.ctfp)
+
+plt.subplot(5,5,11)
+plt.scatter(np.log(yphc), dfc.avh)
+
+plt.subplot(5,5,12)
+plt.scatter(np.log(yphc), dfc.cn)
+
+plt.subplot(5,5,13)
+plt.scatter(np.log(yphc), dfc.hc)
+
+plt.subplot(5,5,14)
+plt.scatter(np.log(yphc), 1-dfc['labsh'])
+
+plt.subplot(5,5,15)
+plt.scatter(np.log(yphc), dfc.ctfp)
+
+plt.subplot(5,5,16)
+plt.scatter(np.log(yphhc), dfc.avh)
+
+plt.subplot(5,5,17)
+plt.scatter(np.log(yphhc), dfc.cn)
+
+plt.subplot(5,5,18)
+plt.scatter(np.log(yphhc), dfc.hc)
+
+plt.subplot(5,5,19)
+plt.scatter(np.log(yphhc), 1-dfc['labsh'])
+
+plt.subplot(5,5,20)
+plt.scatter(np.log(yphhc), dfc.ctfp)
+
+plt.subplot(5,5,21)
+plt.scatter(np.log(ypc), dfc.avh)
+
+plt.subplot(5,5,22)
+plt.scatter(np.log(ypc), dfc.cn)
+
+plt.subplot(5,5,23)
+plt.scatter(np.log(ypc), dfc.hc)
+
+plt.subplot(5,5,24)
+plt.scatter(np.log(ypc), 1-dfc['labsh'])
+
+plt.subplot(5,5,25)
+plt.scatter(np.log(ypc), dfc.ctfp)
+
 plt.show()
+#measures of success
 
